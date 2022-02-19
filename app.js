21b82ec424891f17bc6e174409846dcdd8734fbd
Fernando Ricárdez <hec_montemoon@hotmail.com>
2022-02-19 05:07:21 -0600

    home page

--- app.js
+++ app.js
@@ -26,13 +26,25 @@ function init() {
 }
 
 
-function changeUrl() {
-    urlBar.value = iframeBody.contentWindow.location.href;
-}
 
-function search(ev) {
-    console.log
+function search(ev) {    
     if (ev.key=='Enter') {
-        iframeBody.src=urlBar.value;
+        let text = urlBar.value
+        if (!text || text==="") return;
+        let test = /^https:.*/.test(urlBar.value);
+        if (test) {    
+            iframeBody.src=urlBar.value;
+            return;
+        }
+        test=/\.(org|com|net|mx)/.test(urlBar.value);
+        if (test) {    
+            iframeBody.src="https://"+urlBar.value;
+            return;
+        }
+        text.replaceAll(" ", "%20");
+        console.log("https://google.com/search?q="+text+"&igu=1");
+        let t
+        iframeBody.src="https://google.com/search?q="+text+"&igu=1";
+
     }
 }
