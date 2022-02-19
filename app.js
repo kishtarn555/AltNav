01d1c280e8513a15fe1cb4faa9c4e30176025c6d
Fernando Ricárdez <hec_montemoon@hotmail.com>
2022-02-18 22:58:07 -0600

    changes to app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+
+window.onload=init;
+
 let deferredPrompt;
-let btnInstall =document.getElementById('btnInstall');
+
+let btnInstall;
 window.addEventListener('beforeinstallprompt', (e)=> {
     e.preventDefault();
     deferredPrompt=e;
     btnInstall.style.display="block";
 });
+function init() {
+    btnInstall =document.getElementById('btnInstall');
+    
 
-btnInstall.addEventListener('click', (e)=> {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((result)=> {
-        deferredPrompt=null;
+    btnInstall.addEventListener('click', (e)=> {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((result)=> {
+            deferredPrompt=null;
+        })
     })
-})
+}
