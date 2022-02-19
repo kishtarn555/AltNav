565cd67b43e126beb314f76fe58cbd6509f01ca4
Fernando Ricárdez <hec_montemoon@hotmail.com>
2022-02-18 23:57:16 -0600

    cache in sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,13 @@
 const staticAltNav = "alt-nav-site";
 const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/logo/48x48.png",
+  "/logo/72x72.png",
+  "/logo/96x96.png",
+  "/logo/AltNav512.png"
 ]
 
 self.addEventListener("install", installEvent=> {
