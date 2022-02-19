86c8b9137a80c962126e3eb8b7ac0b7187c4284b
Fernando Ricárdez <hec_montemoon@hotmail.com>
2022-02-18 22:50:02 -0600

    changes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("its alive");
+let deferredPrompt;
+let btnInstall =document.getElementById('btnInstall');
+window.addEventListener('beforeinstallprompt', (e)=> {
+    e.preventDefault();
+    deferredPrompt=e;
+    btnInstall.style.display="block";
+});
+
+btnInstall.addEventListener('click', (e)=> {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((result)=> {
+        deferredPrompt=null;
+    })
+})
