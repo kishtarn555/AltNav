4c369deb44e8da610d70545cf3aee5357257d3d8
kishtarn555 <hec_montemoon@hotmail.com>
2022-02-18 21:48:50 -0600

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,12 @@
+const staticAltNav = "alt-nav-site";
+const assets = [
+]
+
+self.addEventListener("install", installEvent=> {
+  installEvent.waitUntil(
+    caches.open(staticAltNav).then(cache> {
+      cache.addAll(assets)
+    })
+   )
+  
+})
